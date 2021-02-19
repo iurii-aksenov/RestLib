4941cceb9561252d491931ce9d8a0857d8c9b7d5
Iurii Aksenov <iurii.aksenov@yandex.ru>
2021-02-19 18:00:26 +0700

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
